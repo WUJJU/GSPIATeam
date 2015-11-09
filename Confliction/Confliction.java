c5796d52a4a871527398a1b023e0e6025ce033df
WUJJU <haowu90@gmail.com>
2015-11-09 09:44:53 -0500

    update double N to String

--- Confliction/Confliction.java
+++ Confliction/Confliction.java
@@ -10,7 +10,7 @@ import java.util.Set;
     public class DoublePQ {
         private DoubleCar [] pq;                    // store items at indices 1 to N
         
-        private double N;                       // number of items on priority queue
+        private String N;                       // number of items on priority queue
         private int P1;//postion in pindexpq
         private int P2;//positon in mindexpq
         private HashMap<String,Integer> pindexpq=new HashMap<String,Integer>();//store position ranked by price in PQ
